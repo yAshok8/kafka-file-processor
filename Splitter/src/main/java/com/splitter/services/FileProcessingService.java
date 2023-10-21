80ed1185e1969fa473b3ecc07f985f0bba1a9c24
Ashok <44674369+yAshok8@users.noreply.github.com>
2023-10-21 12:21:43 +0530

    Update FileProcessingService.java
    added logic for file processing

--- Splitter/src/main/java/com/splitter/services/FileProcessingService.java
+++ Splitter/src/main/java/com/splitter/services/FileProcessingService.java
@@ -1,5 +1,8 @@
 package com.splitter.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,13 +11,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Service
 public class FileProcessingService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileProcessingService.class);
+
+    @Autowired
+    private KafkaProducerService producerService;
+
     public void processFile(final MultipartFile file) throws IOException {
+        LOGGER.info("Started processing the file content.");
         final List<String> headers = new ArrayList<>();
         final List<String> values = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
@@ -26,6 +34,8 @@ public class FileProcessingService {
                     headers.addAll(Arrays.asList(fields));
                 else
                     values.addAll(Arrays.asList(fields));
+//                Map<String, String> studentMap = StudentUtilitility.convertToMap(headers, values);
+                producerService.sendMessage(values.toString());
                 rowLine++;
                 values.clear();
             }
