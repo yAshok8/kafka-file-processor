346f36ac72cc69c7488a086f7c894e25c1c13d56
yAshok8 <yashok42@gmail.com>
2023-10-23 10:56:03 +0530

    Minor change

--- Splitter/src/main/java/com/splitter/services/KafkaProducerService.java
+++ Splitter/src/main/java/com/splitter/services/KafkaProducerService.java
@@ -1,5 +1,7 @@
 package com.splitter.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
@@ -8,11 +10,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaProducerService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducerService.class);
+
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
     @Async
     public void sendMessage(String message) {
+        LOGGER.info("Sending message to kafka {}", message);
 //        kafkaTemplate.send("your-topic-name", message);
         System.out.println();
     }
