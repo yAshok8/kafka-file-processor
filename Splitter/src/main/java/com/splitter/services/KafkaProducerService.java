8ab880cca3f58e4492adf720bfb72b86e021c60f
yAshok8 <yashok42@gmail.com>
2023-10-24 18:57:05 +0530

    added topic name in properties file and made producer to produce to this topic

--- Splitter/src/main/java/com/splitter/services/KafkaProducerService.java
+++ Splitter/src/main/java/com/splitter/services/KafkaProducerService.java
@@ -3,6 +3,7 @@ package com.splitter.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,16 @@ public class KafkaProducerService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducerService.class);
 
+    @Value("${spring.kafka.producer.topic-name}")
+    private String topicName;
+
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
     @Async
     public void sendMessage(String message) {
         LOGGER.info("Sending message to kafka {}", message);
-//        kafkaTemplate.send("your-topic-name", message);
+        kafkaTemplate.send(topicName, message);
         System.out.println();
     }
 }
